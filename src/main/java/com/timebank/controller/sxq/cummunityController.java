5e3ba8c2ad4066add148a9eef61656867f8202cd
王泽东 <977758778@qq.com>
2018-06-04 15:03:52 +0800

    Wzd:第一次提交

--- src/main/java/com/timebank/controller/sxq/cummunityController.java
+++ src/main/java/com/timebank/controller/sxq/cummunityController.java
@@ -29,7 +29,7 @@ public class cummunityController {
     @Autowired
     private UsersMapper usersMapper;
 
-
+//test测试
     @RequestMapping(value = "/createCommunityView")
     public String createCommunityView(Model model){
 
