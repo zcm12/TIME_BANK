55618b1f9bca438074b4f93fcc79a71e7a2ea788
wbw123 <429565832@qq.com>
2018-07-06 15:02:43 +0800

    nothing

--- src/main/java/com/timebank/shiro/ShrioRegister.java
+++ src/main/java/com/timebank/shiro/ShrioRegister.java
@@ -34,7 +34,7 @@ public class ShrioRegister {
     private   int hashIterations=10;
     private boolean flag=true;
 
-    public boolean register(Users users) {
+    public boolean register1(Users users) {
 
         System.out.println("密码加密开始");
         //干扰数据 盐 防破解
